af5c6dfea0555d98f824be1666d48bc6aa2a2b6c
Дмитрий Монголин 8К43 <dsm45@tpu.ru>
2025-11-12 17:00:52 +0700

    Был сверстан экран MainMenu

---
+++ app/src/main/java/com/example/playlistmaker/PlaylistsActivity.kt
@@ -0,0 +1,14 @@
+package com.example.playlistmaker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class PlaylistsActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
