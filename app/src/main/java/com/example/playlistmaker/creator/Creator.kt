78c5ac6a1bca5b26b2acf2a2188c9aca1561b239
Дмитрий Монголин 8К43 <dsm45@tpu.ru>
2026-04-13 02:21:25 +0700

    Создал PlaylistsViewModel

--- app/src/main/java/com/example/playlistmaker/creator/Creator.kt
+++ app/src/main/java/com/example/playlistmaker/creator/Creator.kt
@@ -1,7 +1,10 @@
 package com.example.playlistmaker.creator
 
+import DatabaseMock
+import com.example.playlistmaker.data.PlaylistRepositoryImpl
 import com.example.playlistmaker.data.SearchHistoryRepositoryImpl
 import com.example.playlistmaker.data.TracksRepositoryImpl
+import com.example.playlistmaker.domain.PlaylistsRepository
 import com.example.playlistmaker.domain.TracksRepository
 
 import com.example.playlistmaker.domain.SearchHistoryRepository
@@ -13,6 +16,8 @@ object Creator {
     private val appScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
     private var historyRepository: SearchHistoryRepository? = null
     private var tracksRepository: TracksRepository? = null
+    private var playlistsRepository: PlaylistsRepository? = null
+    private var databaseRepository: DatabaseMock? = null
 
     fun getSearchHistoryRepository(): SearchHistoryRepository {
         if (historyRepository == null) {
@@ -26,5 +31,19 @@ object Creator {
             tracksRepository = TracksRepositoryImpl(scope = appScope)
         }
         return tracksRepository!!
+    }
+
+    fun getPlaylistRepository(): PlaylistsRepository {
+        if (playlistsRepository == null) {
+            playlistsRepository = PlaylistRepositoryImpl(scope = appScope)
+        }
+        return playlistsRepository!!
+    }
+
+    fun getDataBaseRepository(): DatabaseMock {
+        if (databaseRepository == null) {
+            databaseRepository = DatabaseMock(scope = appScope)
+        }
+        return databaseRepository!!
     }
 }
--- app/src/main/java/com/example/playlistmaker/ui/view_model/PlaylistsViewModel.kt
+++ app/src/main/java/com/example/playlistmaker/ui/view_model/PlaylistsViewModel.kt
@@ -2,8 +2,17 @@ package com.example.playlistmaker.ui.view_model
 
 import DatabaseMock
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.playlistmaker.creator.Creator
 import com.example.playlistmaker.domain.PlaylistsRepository
 import com.example.playlistmaker.domain.TracksRepository
+import com.example.playlistmaker.network.Playlist
+import com.example.playlistmaker.network.Track
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.launch
 
 class PlaylistsViewModel(
     private val playlistsRepository: PlaylistsRepository,
@@ -11,4 +20,50 @@ class PlaylistsViewModel(
     private val databaseRepository: DatabaseMock
 ): ViewModel() {
 
+    val playlists: Flow<List<Playlist>> = playlistsRepository.getAllPlaylists() // Потенциально на подумать?
+    val favouriteList: Flow<List<Track>> = databaseRepository.getFavouriteTracks()
+
+    fun createNewPlaylist(namePlaylist: String, description: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            playlistsRepository.addNewPlaylist(namePlaylist,description)
+        }
+    }
+
+    suspend fun insertTrackToPlaylist(track: Track, playlistId: Long) {
+        tracksRepository.insertTrackToPlayList(track, playlistId)
+    }
+
+    suspend fun toggleFavourite(track: Track, isFavourite: Boolean) {
+        tracksRepository.updateTrackFavouriteStatus(track,isFavourite)
+    }
+
+    suspend fun deleteTrackFromPlaylist(track: Track) {
+        tracksRepository.deleteTrackFromPlaylist(track)
+    }
+
+    suspend fun deletePlaylistById(id: Long) {
+        tracksRepository.deleteTracksByPlaylistId(id)
+        playlistsRepository.deletePlaylistById(id)
+    }
+
+    suspend fun isExist(track: Track): Track? {
+        return tracksRepository.getTrackByNameAndArtist(track).firstOrNull()
+    }
+
+    companion object {
+        fun getViewModelFactory(): ViewModelProvider.Factory =
+            object : ViewModelProvider.Factory {
+                @Suppress("UNCHECKED_CAST")
+                override fun <T: ViewModel> create(modelClass: Class<T>): T {
+                    val tracksRepository = Creator.getTracksRepository()
+                    val playlistsRepository = Creator.getPlaylistRepository()
+                    val databaseRepository = Creator.getDataBaseRepository()
+                    return PlaylistsViewModel(
+                        tracksRepository = tracksRepository,
+                        playlistsRepository = playlistsRepository,
+                        databaseRepository = databaseRepository
+                    ) as T
+                }
+            }
+    }
 }
